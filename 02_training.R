8354ffa8b5bf7d3ac0dfcd5dbcff9bc059f13714
Stephen Royle <s.j.royle@warwick.ac.uk>
2025-01-17 17:00:25 +0000

    Update materials

--- 02_training.R
+++ 02_training.R
@@ -1,27 +1,96 @@
+# we will use this script to learn how to use R
+
+# Typically, we would analyse data in ImageJ and then analyse the outputs in R
+# for calculations and visualisation.
+# The R project stage can be divided into three parts:
+# (0. Setup RStudio project)
+# 1. Load data
+# 2. Calculations
+# 3. Visualisation
+
+## 0. Setup RStudio project ----
+## Setup preferred directory structure in wd
+ifelse(!dir.exists("Data"), dir.create("Data"), "Folder exists already")
+ifelse(!dir.exists("Output"), dir.create("Output"), "Folder exists already")
+ifelse(!dir.exists("Output/Data"), dir.create("Output/Data"), "Folder exists already")
+ifelse(!dir.exists("Output/Plots"), dir.create("Output/Plots"), "Folder exists already")
+ifelse(!dir.exists("Script"), dir.create("Script"), "Folder exists already")
+
+## 1. Load data ----
+# read one csv file into R as an object called temp
 temp <- read.csv("Data/control_n1_1.csv")
+# have a look at it
 View(temp)
+# remove the object
 rm(temp)
 
+# we want to load all files, so we need to get a list of all files
 list.files("Data")
+# assign this list as an object
 filelist <- list.files("Data")
+# use the path to the file
 filelist <- list.files("Data", full.names = TRUE)
+
 # load all files and rbind into big dataframe
 df <- do.call(rbind, lapply(filelist, read.csv))
 View(df)
-# load all files and rbind into big dataframe, add a column to each file to identify the file
+# load all files and rbind into big dataframe, add a column to each file to identify each file
 df <- do.call(rbind, lapply(filelist, function(x) {temp <- read.csv(x); temp$file <- x; temp}))
-df <- do.call(rbind, lapply(filelist, function(x) {temp <- read.csv(x); temp$file <- basename(x); temp}))
+# this can be written on multiple lines
+df <- do.call(rbind, lapply(filelist, function(x) {
+  temp <- read.csv(x)
+  temp$file <- x
+  temp
+}))
+# this is close to what we want but remember that we used full paths to load the files, we only want the file name
+# we can use the basename function to extract the file name
+df <- do.call(rbind, lapply(filelist, function(x) {
+  temp <- read.csv(x);
+  temp$file <- basename(x);
+  temp
+}))
 # file column has name of the file, name is of the form foo_bar_1.csv, extract foo and bar into two columns
 df$cond <- sapply(strsplit(df$file, "_"), "[", 1)
 df$expt <- sapply(strsplit(df$file, "_"), "[", 2)
+# explain why the above works
+# strsplit(df$file, "_") returns a list of vectors, each vector is the result of splitting the string by "_"
+# we can extract the first element of each vector using "[", 1
+# we can extract the second element of each vector using "[", 2
+# sapply applies the function to each element of the list
+# what does "[" do? it extracts elements from a vector
+# what does "[[" do? it extracts elements from a list
 
-## Graphing ----
+## 2. Calculations ----
+# normalise the data in the Mean column - just as an example
+df$MeanNorm <- df$Mean / max(df$Mean)
+# standardise the data in the Mean column - just as an example
+df$MeanStand <- (df$Mean - mean(df$Mean)) / sd(df$Mean)
 
+# since we have messed things up a bit, we can remake the df easily
+# run the top part again to remake the df and then save the output
+filelist <- list.files("Data", full.names = TRUE)
+df <- do.call(rbind, lapply(filelist, function(x) {
+  temp <- read.csv(x);
+  temp$file <- basename(x);
+  temp
+}))
+df$cond <- sapply(strsplit(df$file, "_"), "[", 1)
+df$expt <- sapply(strsplit(df$file, "_"), "[", 2)
+# write data to file
+write.csv(df, "Output/Data/df.csv")
+
+## 3. Visualisation ----
+# we will use ggplot2 for visualisation
+# note that library loading usually goes at the top of the script!
 library(ggplot2)
 
+# histogram to look at the data
 ggplot(df, aes(x = Mean, fill = cond)) + geom_histogram()
+# density plot to look at the data
 ggplot(df, aes(x = Mean, fill = cond)) + geom_density(alpha = 0.5)
+# facetting, make plots by expt
 ggplot(df, aes(x = Mean, fill = cond)) + geom_density(alpha = 0.5) + facet_wrap(~expt)
+# themes
 ggplot(df, aes(x = Mean, fill = cond)) + geom_density(alpha = 0.5) + facet_wrap(~expt) + theme_minimal()
 ggplot(df, aes(x = Mean, fill = cond)) + geom_density(alpha = 0.5) + facet_wrap(~expt) + theme_bw()
 
@@ -54,8 +123,8 @@ ggplot(df, aes(x = cond, y = Mean, colour = expt)) +
   theme_bw(10) +
   theme(legend.position = "none")
 ggsave("Output/Plots/output.png", width = 6, height = 4, dpi = 300)
+# this is an easy way
 
-## Analysis ----
-
+# stats
 # t-test
 t.test(summary_df$Mean ~ summary_df$cond)
--- README.md
+++ README.md
@@ -66,7 +66,47 @@ Before we tackle steps 1-3, we have a step 0 which is to set up an R project to 
 We always use the R Project folder as our working directory.
 It makes the project portable and doesn't rely on paths to folders on a specific computer.
 
-### 1-3. Run the script
-
-Execute the script `02_training.R` line by line (cmd + Enter on Mac; ctrl + Enter on Windows/Linux), explaining what each line does as you go.
+**Steps 1-3** execute the script `02_training.R` line by line (cmd + Enter on Mac; ctrl + Enter on Windows/Linux), explaining what each line does as you go.
 Check for understanding throughout.
+
+### 1. Read in the data
+
+**Goal:** make one data frame containing all the data
+
+- Begin by reading in one file into a data frame, explain that we have 80 files to read in.
+- Show how we can read all of them in to one huge data frame using a simple command, but how do we know which rows belong to what condition and/or which experimental repeat?
+- Use the filename to append information as it is read in.
+
+**Key concept:** think about how you'll name the outputs of your analysis in Fiji to make reading the data into R as easy as possible.
+
+An alternative workflow that we use in the lab is to use nested folders for conditions and experimental repeats, rather than a flat structure as in this example.
+In that case, folder names are used to append information to the data frame.
+
+### 2. Do some calculations
+
+This is an optional step.
+Some examples are shown but they are not needed for this exercise, as we will simply plot the data.
+
+### 3. Make some plots
+
+- Use ggplot to make some plots
+- Explain grammar of graphics and demonstrate the power of facetting, theming and so on
+- Explore the data, notice that one experiment is different to the others
+- Make a SuperPlot
+- Save the SuperPlot
+
+**Key concept:** the data frame you made has all the information to make any plot you'd need.
+
+## Homework
+
+To consolidate the learning, ask the participants to figure out how to do the following:
+
+**Which row (from which expt/cond/cell) had the lowest Mean value?**
+
+Explain that the person who did the experiments found out that the rapamycin used in the 4th experiment, was prepared from a stock solution at the wrong concentration.
+
+**How can we exclude the n4 data and remake a new SuperPlot so that all experiments used the correct concentration?**
+
+Explain that getting assistance from an LLM is unlikely to help them learn.
+Searching for a solution is fine.
+Keywords: subset/subsetting
